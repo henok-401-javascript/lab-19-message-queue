2f141ebfbecb5e0657883a20843b28bc54a0c7ce
henok Gebremedhn <henoklwi@gmail.com>
2020-05-20 23:41:21 -0700

    add more work on api.js

--- queue-server/queue-server.js
+++ queue-server/queue-server.js
@@ -2,10 +2,20 @@
 const io = require('socket.io');
 const server = io(3001);
 
+let queue = {};
+console.log('Message Queue Server up and running on 3001');
 server.on('connection', (socket) => {
-  console.log('socket connection api', socket.id);
-  socket.on('delivery-order', (payload) => {
+  console.log('connected ', socket.id);
+
+  socket.on('delivered', (payload) => {
     console.log('order saved in the queue', payload);
-    socket.emit('flowerOrder-queued', payload);
+
+    if (queue[payload.vendor]) {
+      queue[payload.vendor].push([payload.vendor.id]);
+      server.to(payload.vendor).emit('queue', queue[payload.vendor]);
+    } else {
+      queue[payload.vendor] = [payload.vendor];
+      server.to(payload.vendor).emit('queue', queue[payload.vendor]);
+    }
   });
 });
--- vendor-02/vendor-02.js
+++ vendor-02/vendor-02.js
@@ -1,3 +1,10 @@
 'use strict';
-// const sic = require('socket.io-client');
-// const candySocket = sic.connect('http://localhost:3000');
+const sic = require('socket.io-client');
+const candySocket = sic.connect('http://localhost:3001');
+
+candySocket.on('queue', (payload) => {
+  if (payload && payload.Id) {
+    console.log('Thank you for delivering order', payload.Id);
+    candySocket.emit('received', 'candyVendor');
+  }
+});
